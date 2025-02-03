3074ad780c13c06df0ee02605789aceffc0fcb2b
Shinya ISHIKAWA <s-ishikawa@ugo.plus>
2025-02-03 12:14:23 +0900

    基底クラスMoneyを実装

--- src/money.ts
+++ src/money.ts
@@ -1,25 +1,21 @@
-export class Dollar {
-  #amount: number;
+class Money {
+  protected amount: number;
   constructor(amount: number) {
-    this.#amount = amount;
+    this.amount = amount;
   }
-  times(multiplier: number): Dollar {
-    return new Dollar(this.#amount * multiplier)
-  }
-  equals(dollar: Dollar): boolean {
-    return this.#amount === dollar.#amount;
+  public equals(money: Money): boolean {
+    return this.amount === money.amount;
   }
 }
 
-export class Franc {
-  #amount: number;
-  constructor(amount: number) {
-    this.#amount = amount;
+export class Dollar extends Money {
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
   }
+}
+
+export class Franc extends Money {
   times(multiplier: number): Franc {
-    return new Franc(this.#amount * multiplier)
-  }
-  equals(dollar: Franc): boolean {
-    return this.#amount === dollar.#amount;
+    return new Franc(this.amount * multiplier);
   }
 }
