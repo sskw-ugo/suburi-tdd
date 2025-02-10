cb90efa5c2290513f9fcca756954ea2787f4d95f
Shinya ISHIKAWA <s-ishikawa@ugo.plus>
2025-02-10 12:38:03 +0900

    11章まで読んだ

--- src/money.test.ts
+++ src/money.test.ts
@@ -9,35 +9,25 @@ function assertEquals<T>(foo: Equals<T>, bar: T) {
   expect(foo.equals(bar)).toBe(true)
 }
 
-describe('Dollar', () => {
+describe('Money', () => {
   it('掛け算ができる', () => {
-    const five: Money = Money.dollar(5);
+    let five: Money = Money.dollar(5);
     assertEquals(Money.dollar(10), five.times(2));
     assertEquals(Money.dollar(15), five.times(3));
 
-  })
-
-  it('equalができる', () => {
-    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
-    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
-  })
-})
-
-describe('Franc', () => {
-  it('掛け算ができる', () => {
-    const five = Money.franc(5);
+    five = Money.franc(5);
     assertEquals(Money.franc(10), five.times(2));
     assertEquals(Money.franc(15), five.times(3));
   })
 
   it('equalができる', () => {
-    expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
-    expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
-  })
-})
-
-describe('Money', () => {
-  it('equalができる', () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
     expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
+  })
+
+  it('currencyが取得できる', () => {
+    expect(Money.dollar(1).currency).toBe('USD');
+    expect(Money.franc(1).currency).toBe('CHF');
   })
 })
--- src/money.ts
+++ src/money.ts
@@ -1,30 +1,51 @@
-export abstract class Money {
-  static dollar(amount: number): Dollar {
-    return new Dollar(amount);
+type Brand = "USD" | "CHF";
+
+interface MoneyMapping {
+  USD: Money;
+  CHF: Money;
+}
+
+export class Money {
+  // ジェネリクスを使い、brandに応じた型を返すように定義
+  static create<B extends Brand>(brand: B, amount: number): MoneyMapping[B] {
+    switch (brand) {
+      case "USD":
+        // 型アサーションを用いて MoneyMapping[B] に合わせる
+        return new Money(amount, "USD") as MoneyMapping[B];
+      case "CHF":
+        return new Money(amount, "CHF") as MoneyMapping[B];
+      default:
+        // never を利用して網羅性チェック
+        // biome-ignore lint/correctness/noSwitchDeclarations: <explanation>
+        const _exhaustiveCheck: never = brand;
+        throw new Error("Invalid brand");
+    }
   }
-  static franc(amount: number): Franc {
-    return new Franc(amount);
-  }
+
   protected amount: number;
-  constructor(amount: number) {
+  protected _currency: Brand;
+  constructor(amount: number, currency: Brand) {
     this.amount = amount;
+    this._currency = currency;
+  }
+
+  static dollar(amount: number): Money {
+    return Money.create("USD", amount);
+  }
+  static franc(amount: number): Money {
+    return Money.create("CHF", amount);
+  }
+
+  get currency(): Brand {
+    return this._currency;
   }
   public equals(money: Money): boolean {
-    return (
-      this.constructor === money.constructor && this.amount === money.amount
-    );
+    return this.currency === money.currency && this.amount === money.amount;
   }
-  abstract times(multiplier: number): Money;
-}
-
-export class Dollar extends Money {
-  times(multiplier: number): Money {
-    return new Dollar(this.amount * multiplier);
+  public times(multiplier: number): Money {
+    return new Money(this.amount * multiplier, this._currency);
   }
 }
 
-export class Franc extends Money {
-  times(multiplier: number): Money {
-    return new Franc(this.amount * multiplier);
-  }
-}
+
+export class Franc extends Money {}
