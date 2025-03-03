2d33b68631748bf79bb8327c87bebb6bb134763c
Shinya ISHIKAWA <s-ishikawa@ugo.plus>
2025-03-03 12:54:14 +0900

    WIP: 23章の途中

--- src/part2_xunit/xunit.ts
+++ src/part2_xunit/xunit.ts
@@ -7,13 +7,19 @@ class TestCase {
     this.name = name;
   }
 
-  run() {
+  run(result: TestResult = new TestResult()) {
+    result.testStarted();
     this.setUp();
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    const method = (this as any)[this.name] as () => void;
-    method.call(this);
+    try {
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      const method = (this as any)[this.name] as () => void;
+      method.call(this);
+    } catch {
+      result.testFailed();
+    }
 
     this.tearDown();
+    return result;
   }
 
   setUp() {
@@ -32,6 +38,10 @@ class WasRun extends TestCase {
     this.log += "testMethod ";
   }
 
+  testBrokenMethod() {
+    throw new Error();
+  }
+
   override setUp() {
     this.log = "setUp ";
   }
@@ -41,12 +51,75 @@ class WasRun extends TestCase {
   }
 }
 
+class TestResult {
+  runCount = 0;
+  errorCount = 0;
+
+  testStarted() {
+    this.runCount += 1;
+  }
+
+  testFailed() {
+    this.errorCount += 1;
+  }
+
+  summary() {
+    return `${this.runCount} run, ${this.errorCount} failed`;
+  }
+}
+
 class TestCaseTest extends TestCase {
   testTemplateMethod() {
     const test = new WasRun("testMethod");
     test.run();
     assert("setUp testMethod tearDown " === test.log);
   }
+
+  testResult() {
+    const test = new WasRun("testMethod");
+    const result = test.run();
+    assert("1 run, 0 failed" === result.summary());
+  }
+
+  testFailedResult() {
+    const test = new WasRun("testBrokenMethod");
+    const result = test.run();
+    assert("1 run, 1 failed" === result.summary());
+  }
+
+  testFailedResultFormatting() {
+    const result = new TestResult();
+    result.testStarted();
+    result.testFailed();
+    assert("1 run, 1 failed" === result.summary());
+  }
+
+  testSuite() {
+    const suite = new TestSuite();
+    suite.add(new WasRun("testMethod"));
+    suite.add(new WasRun("testBrokenMethod"));
+    const result = suite.run();
+    assert("2 run, 1 failed" === result.summary());
+  }
 }
 
-new TestCaseTest("testTemplateMethod").run();
+class TestSuite {
+  tests: TestCase[] = [];
+
+  add(test: TestCase) {
+    this.tests.push(test);
+  }
+
+  run() {
+    const result = new TestResult();
+    for (const test of this.tests) {
+      test.run(result);
+    }
+    return result;
+  }
+}
+
+console.log(new TestCaseTest("testTemplateMethod").run().summary());
+console.log(new TestCaseTest("testResult").run().summary());
+console.log(new TestCaseTest("testFailedResult").run().summary());
+console.log(new TestCaseTest("testFailedResultFormatting").run().summary());
